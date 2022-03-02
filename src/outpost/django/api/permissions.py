3871d51ef197d694a390130316c727206a94b584
Michael Fladischer <michael@fladi.at>
2022-03-02 17:17:13 +0100

    Use Python 3.9 dict merge syntax.

--- src/outpost/django/api/permissions.py
+++ src/outpost/django/api/permissions.py
@@ -3,15 +3,13 @@ from rest_framework.permissions import DjangoModelPermissions, DjangoObjectPermi
 
 class ExtendedDjangoModelPermissions(DjangoModelPermissions):
 
-    perms_map = dict(
-        list(DjangoModelPermissions.perms_map.items())
-        + [("GET", ["%(app_label)s.view_%(model_name)s"])]
-    )
+    perms_map = DjangoModelPermissions.perms_map | {
+        "GET": ["%(app_label)s.view_%(model_name)s"],
+    }
 
 
 class ExtendedDjangoObjectPermissions(DjangoObjectPermissions):
 
-    perms_map = dict(
-        list(DjangoObjectPermissions.perms_map.items())
-        + [("GET", ["%(app_label)s.view_%(model_name)s"])]
-    )
+    perms_map = DjangoObjectPermissions.perms_map | {
+        "GET": ["%(app_label)s.view_%(model_name)s"],
+    }
